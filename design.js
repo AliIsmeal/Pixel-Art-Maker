c2e19e34c4af41a2550447712535f300374d9b55
Ali Developer <aliameer1977@gmail.com>
2018-02-09 18:49:29 -0700

    Pixel Art Maker

---
+++ design.js
@@ -0,0 +1,31 @@
+﻿function start(event) {
+    //to prevent the default behavior of the browser and call the makeGrid function
+    event.preventDefault();
+    makeGrid();
+}
+
+function makeGrid() {
+
+    var table, gridHeight, gridWidth;
+    // get the input values for the grid size
+    table = $('#pixel_canvas');
+    gridHeight = $('#input_height').val();
+    gridWidth = $('#input_width').val();
+
+    // remove table children each time the user enter new values for the size of the grid gridHeight and gridWidth.
+    table.children().remove();
+    //Construct the grid based on the values of 
+    for (i = 0; i < gridHeight; i++) {
+        table.append('<tr class="grid-Row"></tr>');
+    }
+
+    for (j = 0; j < gridWidth; j++) {
+        $('.grid-Row').append('<td class="grid-Cell"></td>');
+    } 	// When td is clicked by the user, change color of td
+    $('.grid-Cell').click(function () {
+
+        $(this).attr('bgcolor', $("#color-Picker").val());
+    });
+}
+
+
